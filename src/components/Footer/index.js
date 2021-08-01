9d2697f7d69b5b24d2d2911d5d26dbb120332630
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-08-01 19:40:44 +0100

    added social links to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,26 @@
+import { FaGithubSquare, FaLinkedin, FaInstagram } from "react-icons/fa";
+
 import "./Footer.css";
+import IconLink from "../IconLink";
 
 const Footer = () => {
   return (
     <footer class="footer border-top py-3 text-center">
       <div>Designed and Built by Natasha Mann</div>
+      <div className="contact-icons mx-auto">
+        <IconLink
+          logo={<FaLinkedin size="2rem" className="contact-logo" />}
+          link="https://www.linkedin.com/in/natashamann2896/"
+        />
+        <IconLink
+          logo={<FaGithubSquare size="2rem" className="contact-logo" />}
+          link="http://www.github.com/natasha-mann"
+        />
+        <IconLink
+          logo={<FaInstagram size="2rem" className="contact-logo" />}
+          link="http://www.github.com/natasha-mann"
+        />
+      </div>
     </footer>
   );
 };
